d10200c3eaca2ed49b9082e312ce35b8a32c7e34
YourGitHubUsername <youremail@example.com>
2026-02-12 16:55:09 +0200

    added index.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city){
+let apiKey = "eea6ff3f83446t880010aod1f92b9fab";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey} `;
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    sea
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
