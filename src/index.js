48e7aa88cae8115d28ebc90574a58ca4e6c8abd2
YourGitHubUsername <youremail@example.com>
2026-02-13 18:57:57 +0200

    added html and javascript

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-function searchCity(city){
-let apiKey = "eea6ff3f83446t880010aod1f92b9fab";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey} `;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+   let timeElement = document.querySelector("#time");
+
+
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = "Tuesday 14:49";
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "eea6ff3f83446t880010aod1f92b9fab";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    sea
-searchCity(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+// Default city on page load
+searchCity("Johannesburg");
